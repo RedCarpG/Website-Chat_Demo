33d3e12e718af4b0d33909b85ff4d976a16785ec
RedCarpG <redcarpg@gmail.com>
2022-03-25 00:18:00 +0100

    fix: chat room bug fixed, separate components

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,49 +7,20 @@ import { useCurrentUser, useSignIn } from "../utils/database";
 import ChatWindow from "../components/ChatWindow";
 import ProfileWindow from "../components/ProfileWindow";
 import Footer from "../components/Footer";
-
-const SignIn: React.FC = () => {
-  const [signInWithGoogle, user, loading, error] = useSignIn();
-
-  const customParameters = { login_hint: "user@example.com" };
-  if (error) {
-    return (
-      <div>
-        <p>Error: {error.message}</p>
-      </div>
-    );
-  }
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-  if (user) {
-    return (
-      <div>
-        <p>Signed In User: {user.providerId}</p>
-      </div>
-    );
-  }
-  return (
-    <button
-      onClick={() => {
-        signInWithGoogle(undefined, customParameters);
-      }}
-    >
-      Sign in with Google
-    </button>
-  );
-};
+import SignIn from "../components/LogIn";
 
 const ChatDemo: NextPage = () => {
   const [user] = useCurrentUser();
   return (
     <>
       <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
         <title>Chat Demo</title>
       </Head>
       <div className={styles.chat_demo}>
         {user ? (
           <>
+            <h1 className={styles.title}>Global Chat Room Demo</h1>
             <ChatWindow />
             <ProfileWindow uid={user.uid} />
           </>
