2f4198e8d791cffe29af32be1ec3464e9e0e4367
RedCarpG <redcarpg@gmail.com>
2022-03-25 12:56:21 +0100

    fix: index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 
-import { useCurrentUser, useSignIn } from "../utils/database";
+import { useCurrentAuthUser } from "../utils/database";
 
 import ChatWindow from "../components/ChatWindow";
 import ProfileWindow from "../components/ProfileWindow";
@@ -10,7 +10,8 @@ import Footer from "../components/Footer";
 import SignIn from "../components/LogIn";
 
 const ChatDemo: NextPage = () => {
-  const [user] = useCurrentUser();
+  const [user] = useCurrentAuthUser();
+
   return (
     <>
       <Head>
@@ -20,7 +21,7 @@ const ChatDemo: NextPage = () => {
       <div className={styles.chat_demo}>
         {user ? (
           <>
-            <h1 className={styles.title}>Global Chat Room Demo</h1>
+            <h1 className={styles.title}>{process.env.projectTitle}</h1>
             <ChatWindow />
             <ProfileWindow uid={user.uid} />
           </>
